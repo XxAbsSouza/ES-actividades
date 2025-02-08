c6b2aa30b83ae5305cb83d21c5e3402e77513b46
Ana Beatriz  dos Santos Souza <beatriz_ana58@hotmail.com>
2025-02-08 19:48:06 -0300

    concluído

--- El Salvador Page/js/script.js
+++ El Salvador Page/js/script.js
@@ -1,6 +1,7 @@
 const primaryNav = document.querySelector('.primary-navigation');
 const navToggle = document.querySelector('.mobile-nav-toggle');
 const navLinks = document.querySelectorAll('.primary-navigation a');
+const headerTittle = document.querySelector('.header-tittle');
 
 navToggle.addEventListener('click', () => {
     const visibility = primaryNav.getAttribute('data-visible');
@@ -36,19 +37,41 @@ navLinks.forEach(link => {
 });
 
 let lastScrollTop = 0;
-navbar = document.getElementById('navbar');
-window.addEventListener('scroll', function() {
-    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    if (scrollTop>lastScrollTop) {
-        navbar.style.top = "-80px";
+const navbar = document.getElementById('navbar');
+let timeout;
+
+window.addEventListener('scroll', () => {
+    let scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+    if (scrollTop < lastScrollTop) {
+        navbar.classList.add('active');
+
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+            navbar.classList.remove('active');
+        }, 5000);
+    } 
+    else {
+        navbar.classList.remove('active');
         
-    } else {
-        navbar.style.top = "0";
+        clearTimeout(timeout);
     }
-    lastScrollTop = scrollTop;
+
+    if (scrollTop === 0) {
+        navbar.classList.remove('active');
+        clearTimeout(timeout);
+    }
+
+    if (navToggle.getAttribute('aria-expanded') === 'true') {
+        navbar.classList.add('active');
+        clearTimeout(timeout);
+    }
+
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
 });
 
 
-// ---------------------------
+
+
 
 
